1147145ab6933634e55f8e07526bd9ab6d036342
Argyris <arkatsy@gmail.com>
2023-12-31 03:41:48 +0200

    new ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import { Progress } from "@/components/ui/progress";
 import { Tabs, TabsList, TabsContent, TabsTrigger } from "@radix-ui/react-tabs";
-import { type TimerId, defaultTimerTabs, formatTime } from "@/lib/utils";
+import { type TimerId, defaultTimerTabs, formatTime, cn } from "@/lib/utils";
 import { Button } from "./components/ui/button";
 import useCountdown from "./hooks/useCountdown";
-import { useState } from "react";
-import { PauseIcon, PlayIcon, RotateCcwIcon } from "lucide-react";
+import { ComponentProps, useState } from "react";
+import { FastForward, PauseIcon, PlayIcon, RotateCcwIcon } from "lucide-react";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./components/ui/tooltip";
+import { Card, CardHeader, CardTitle } from "./components/ui/card";
 
 export default function App() {
   const [tabs, setTabs] = useState(defaultTimerTabs);
@@ -27,6 +29,12 @@ export default function App() {
     setTabs((tabs) => tabs.map((tab) => ({ ...tab, isActive: tab.id === tabId })));
     setSession(getTabSession(tabId));
   };
+  const moveToNextTab = () => {
+    const activeTabIndex = tabs.findIndex((tab) => tab.isActive);
+    const nextTabIndex = (activeTabIndex + 1) % tabs.length;
+    handleTabChange(tabs[nextTabIndex].id);
+  };
+
   const handlePlaybackButtonClick = () => (isIdle ? begin() : isRunning ? pause() : resume());
 
   const isRunning = status === "running";
@@ -37,52 +45,107 @@ export default function App() {
   const activeTabId = tabs.find((tab) => tab.isActive)!.id;
   const tw_iconSizes = "w-6 h-6";
 
+  const shouldDisableTabs = !isIdle;
+
+  const isBreak = !(activeTabId === "pomodoro");
+
   return (
-    <ThemeProvider defaultTheme="system">
-      <div className="flex flex-col items-center min-h-dvh gap-72">
-        <Progress
-          value={progress}
-          className={`rounded-none h-2 text-primary ${isPaused && "text-primary animate-pulse"}`}
-        />
-        <Tabs className="w-[500px]" value={activeTabId} onValueChange={handleTabChange}>
-          <TabsList className="grid w-full grid-cols-3 p-1.5 items-center justify-center rounded-md bg-muted text-muted-foreground">
-            {tabs.map(({ id, name }) => (
-              <TabsTrigger
-                key={id}
-                value={id}
-                className="inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-2 text-md font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm"
-              >
-                {name}
-              </TabsTrigger>
-            ))}
-          </TabsList>
-          <TabsContent
-            value={activeTabId}
-            tabIndex={-1}
-            className="flex gap-16 flex-col justify-center items-center ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2"
-          >
-            <span className="text-[7rem] font-semibold mt-16">{formatTime(time)}</span>
-            <div className="flex gap-8">
-              <Button
-                variant="outline"
-                className="font-bold py-6 text-2xl uppercase tracking-wider"
-                onClick={handlePlaybackButtonClick}
-              >
-                <span>
-                  {isIdle || isPaused ? (
-                    <PlayIcon className={tw_iconSizes} />
-                  ) : (
-                    <PauseIcon className={tw_iconSizes} />
-                  )}
-                </span>
-              </Button>
-              <Button variant="outline" onClick={handleResetButtonClick} className="py-6">
-                <RotateCcwIcon className={tw_iconSizes} />
-              </Button>
-            </div>
-          </TabsContent>
-        </Tabs>
+    <ThemeProvider defaultTheme="dark">
+      <div className="flex min-h-dvh items-center justify-center gap-72">
+        <Card className="p-10">
+          <CardHeader className="px-0 pb-10">
+            <CardTitle className="text-center text-3xl">Pomodoro</CardTitle>
+          </CardHeader>
+          <Tabs className="w-[500px]" value={activeTabId} onValueChange={handleTabChange}>
+            <TabsList className="grid w-full grid-cols-3 items-center justify-center rounded-md bg-muted p-1.5 text-muted-foreground">
+              {tabs.map(({ id, name }) => (
+                <TabsTrigger
+                  key={id}
+                  value={id}
+                  className="text-md inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-2 font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm"
+                  disabled={shouldDisableTabs}
+                >
+                  {name}
+                </TabsTrigger>
+              ))}
+            </TabsList>
+            <TabsContent
+              value={activeTabId}
+              tabIndex={-1}
+              className="mt-16 flex flex-col items-center justify-center ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2"
+            >
+              <span className="text-[7rem] font-semibold">{formatTime(time)}</span>
+              <div className="flex w-full flex-col gap-16">
+                <Progress
+                  value={progress}
+                  max={100}
+                  className={`h-2 rounded-md text-primary ${
+                    isPaused && "animate-pulse text-primary"
+                  }`}
+                />
+                <div className="flex w-full justify-center gap-8">
+                  <ButtonWithTooltip
+                    variant="outline"
+                    className="py-6"
+                    tooltip="Start/Pause timer"
+                    onClick={handlePlaybackButtonClick}
+                  >
+                    <span>
+                      {isIdle || isPaused ? (
+                        <PlayIcon className={tw_iconSizes} />
+                      ) : (
+                        <PauseIcon className={tw_iconSizes} />
+                      )}
+                    </span>
+                  </ButtonWithTooltip>
+                  <ButtonWithTooltip
+                    variant="outline"
+                    className="py-6"
+                    tooltip="Reset timer"
+                    onClick={handleResetButtonClick}
+                  >
+                    <RotateCcwIcon className={tw_iconSizes} />
+                  </ButtonWithTooltip>
+                  <ButtonWithTooltip
+                    variant="outline"
+                    className="py-6"
+                    tooltip={isBreak ? "Skip break" : "Skip pomodoro"}
+                    onClick={moveToNextTab}
+                  >
+                    <FastForward className={tw_iconSizes} />
+                  </ButtonWithTooltip>
+                </div>
+              </div>
+            </TabsContent>
+          </Tabs>
+        </Card>
       </div>
     </ThemeProvider>
+  );
+}
+
+function ButtonWithTooltip({
+  children,
+  tooltip,
+  className,
+  ...props
+}: {
+  children: React.ReactNode;
+  className?: string;
+  tooltip: string;
+} & ComponentProps<typeof Button>) {
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button className={className} {...props}>
+            {children}
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>{tooltip}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 }
