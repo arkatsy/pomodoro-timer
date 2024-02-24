092c3acab73b23356930b4aff94fa85af7ac8595
Argyris <arkatsy@gmail.com>
2024-02-25 00:53:09 +0200

    feat: add reduced motion support

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { ThemeProvider } from "./components/theme-provider.tsx";
+import { MotionConfig } from "framer-motion";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <ThemeProvider>
-    <App />
+    <MotionConfig reducedMotion="user">
+      <App />
+    </MotionConfig>
   </ThemeProvider>,
 );
