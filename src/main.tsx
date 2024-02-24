7fabf2ebd02527d4e235cfecf8a21315068e02ce
Argyris <arkatsy@gmail.com>
2024-02-25 01:42:24 +0200

    feat: add dev only component for conveniences

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,29 @@
 import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
 import App from "./App.tsx";
 import "./index.css";
 import { ThemeProvider } from "./components/theme-provider.tsx";
 import { MotionConfig } from "framer-motion";
 
+const __DEV__ = import.meta.env.DEV;
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <ThemeProvider>
-    <MotionConfig reducedMotion="user">
-      <App />
-    </MotionConfig>
-  </ThemeProvider>,
+  <StrictMode>
+    <ThemeProvider>
+      <MotionConfig reducedMotion="user">
+        <App />
+        {__DEV__ && <Dev />}
+      </MotionConfig>
+    </ThemeProvider>
+  </StrictMode>,
 );
+
+function Dev() {
+  return (
+    <div className="fixed left-0 top-0">
+      <a className="underline" target="_blank" href="https://github.com/arkatsy/pomodoro-timer.git">
+        Github
+      </a>
+    </div>
+  );
+}
