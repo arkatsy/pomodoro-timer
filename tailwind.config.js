0573d37f6d9eaf2fe4d1d5ec56783e429c346a56
Argyris <arkatsy@gmail.com>
2024-02-27 00:03:38 +0200

    chore: add breakpoints for reference

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,13 @@ module.exports = {
       sans: ["Fredoka", "sans-serif"],
     },
     extend: {
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
