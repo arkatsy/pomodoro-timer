c824f11c21ac48c35034fc1f951fbb1eb1a4c36d
Argyris <arkatsy@gmail.com>
2024-02-26 10:26:16 +0200

    feat: add vite analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,32 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "node:path";
+import { visualizer } from "rollup-plugin-visualizer";
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+declare global {
+  interface ProcessEnv {
+    readonly ANALYZE: "true" | undefined;
+  }
+}
+
+export default defineConfig(({}) => {
+  const shouldAnalyze = process.env.ANALYZE === "true";
+
+  return {
+    plugins: [
+      react(),
+      shouldAnalyze &&
+        visualizer({
+          template: "treemap",
+          open: true,
+          gzipSize: true,
+          filename: "dist/stats.html",
+        }),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+  };
 });
